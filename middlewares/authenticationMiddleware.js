e9421e4ee3dcb13b6ba1ccf28edc6c8abe28afeb
pongangairaman-nks <pongangairamannks@gmail.com>
2025-03-12 11:03:40 +0530

    Adding Authentication middleware and user profile api

--- middlewares/authenticationMiddleware.js
+++ middlewares/authenticationMiddleware.js
@@ -1,1 +1,18 @@
-import jwt from "jsonwebtoken"
+import jwt from "jsonwebtoken";
+
+export const authenticationToken = (req, res, next) => {
+  const token = req.header("Authorization")?.split("")[1];
+  if (!token)
+    return res.status(401).json({
+      message: "Access Denied"
+    });
+  try {
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    req.user = decoded;
+    next(); //moves to next middleware or controller
+  } catch (error) {
+    res.status(403).json({
+      error: "Invalid token"
+    });
+  }
+};
